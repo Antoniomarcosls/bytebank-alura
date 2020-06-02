d90552714c4c67bd56ef498481c6da1f3dbcfd73
Antonio Marcos <antoniomarcosleite5@gmail.com>
2020-06-02 07:37:23 -0300

    Antonio 02-06-2020

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
 const diretor = new Diretor("Antonio", 10000, 11122233398);
 diretor.cadastrarSenha("12345");
 const gerente = new Gerente("Roberto", 5000, 22233344499);
+gerente.cadastrarSenha("123");
+const cliente = new Cliente("Antonio", 123456789, "321");
 
-const estaLogado = SistemaAutenticacao.login(diretor, "12345");
+const diretorEstaLogado = SistemaAutenticacao.login(diretor, "12345");
+const gerenteEstaLogado = SistemaAutenticacao.login(gerente, "123");
+const clienteEstaLogado = SistemaAutenticacao.login(cliente, "321");
 
-console.log(estaLogado);
+console.log(diretorEstaLogado, gerenteEstaLogado, clienteEstaLogado);
+
